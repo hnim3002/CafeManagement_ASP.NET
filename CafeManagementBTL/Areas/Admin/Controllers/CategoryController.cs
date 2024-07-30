90207e502e97a5774da45fa74ce4b73f0f1c8c30
longlo2k3 <168855062+longlo2k3@users.noreply.github.com>
2024-07-31 03:58:08 +0700

    Product_Inventory_Category

--- CafeManagementBTL/Areas/Admin/Controllers/CategoryController.cs
+++ CafeManagementBTL/Areas/Admin/Controllers/CategoryController.cs
@@ -1,8 +1,15 @@
-﻿using CafeManagement.DataAccess.Repository.IRepository;
+﻿using CafeManagement.DataAccess.Repository;
+using CafeManagement.DataAccess.Repository.IRepository;
+using CafeManagement.Models.Entities;
+using CafeManagement.Utilities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CafeManagement.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = WebRoles.Web_Admin)]
     public class CategoryController : Controller
     {
 
@@ -12,19 +19,63 @@ namespace CafeManagement.Web.Areas.Admin.Controllers
         {
             _unitOfWork = unitOfWork;
         }
+        [HttpGet]
         public IActionResult List()
         {
             return View();
         }
 
-        //#region API CALLS
-        //[HttpGet]
-        //public async Task<IActionResult> GetAllCafe()
-        //{
-        //    var categorys = await _unitOfWork.Category.GetAllAsync();
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            var categories = await _unitOfWork.Category.GetAllAsync();
 
-        //    return Json(new { data = categorys });
-        //}
-        //#endregion
+            return Json(new { data = categories });
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _unitOfWork.Category.AddAsync(category);
+            await _unitOfWork.SaveAsync();
+            TempData["Success"] = "Cafe created successfully";
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var Categories = await _unitOfWork.Category.GetAsync(u => u.Id == id);
+            return View(Categories);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Category category)
+        {
+
+            _unitOfWork.Category.Delete(category);
+            await _unitOfWork.SaveAsync();
+            TempData["Success"] = "Cafe deleted successfully";
+
+            return RedirectToAction("List");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var Categories = await _unitOfWork.Category.GetAsync(u => u.Id == id);
+            return View(Categories);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            _unitOfWork.Category.Update(category);
+            await _unitOfWork.SaveAsync();
+            TempData["Success"] = "Category updated successfully";
+            return RedirectToAction("List");
+        }
     }
 }
--- CafeManagementBTL/Areas/Admin/Controllers/ProductController.cs
+++ CafeManagementBTL/Areas/Admin/Controllers/ProductController.cs
@@ -1,12 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CafeManagement.DataAccess.Repository.IRepository;
+using CafeManagement.Models.Entities;
+using CafeManagement.Models.ViewModel;
+using CafeManagement.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CafeManagement.Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = WebRoles.Web_Admin)]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(IUnitOfWork unitOfWork, ILogger<ProductController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public IActionResult List()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct()
+        {
+            var products = await _unitOfWork.Product.GetAllProduct();
+            if (products == null)
+            {
+                _logger.LogError($"Product with ID not found.");
+                TempData["Error"] = "Product not found.";
+                return RedirectToAction("List");
+            }
+            return Json(new { data = products });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create(int id)
+        {
+            var product = await _unitOfWork.Product.GetByProductIDAysnc(id);
+            IEnumerable<Product> products = await _unitOfWork.Product.GetAllProduct();
+            IEnumerable<Category> categories = await _unitOfWork.Category.GetAllAsync();
+            ProductVM productVM = new ProductVM()
+            {
+                Product = product,
+                products = products,
+                category = categories
+            };
+            return View(productVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            await _unitOfWork.Product.AddAsync(product);
+            await _unitOfWork.SaveAsync();
+            TempData["Success"] = "Cafe created successfully";
+            return RedirectToAction("List");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _unitOfWork.Product.GetByProductIDAysnc(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with ID {id} not found.");
+                TempData["Error"] = "Product not found.";
+                return RedirectToAction("List");
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (product == null || product.Id <= 0)
+            {
+                _logger.LogError("Edit failed: Product is null or ID is invalid.");
+                TempData["Error"] = "Product not found or invalid.";
+                return RedirectToAction("List");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                // ModelState không hợp lệ
+                _logger.LogError("Edit failed: ModelState is invalid.");
+                TempData["Error"] = "Invalid product data.";
+                return View(product); // Trả về lại trang Edit với thông báo lỗi
+            }
+
+            _logger.LogInformation($"Attempting to Edit product with ID {product.Id}");
+
+            var existingProduct = await _unitOfWork.Product.GetByProductIDAysnc(product.Id);
+            if (existingProduct == null)
+            {
+                _logger.LogError($"Product with ID {product.Id} not found.");
+                TempData["Error"] = "Product not found.";
+                return RedirectToAction("List");
+            }
+
+            // Cập nhật các thuộc tính của sản phẩm
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+            // Thêm các thuộc tính khác nếu có
+
+            _unitOfWork.Product.Update(existingProduct);
+            await _unitOfWork.SaveAsync();
+            _logger.LogInformation("Product Edited successfully");
+
+            TempData["Success"] = "Product Edited successfully";
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _unitOfWork.Product.GetByProductIDAysnc(id);
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Product product)
+        {
+
+            _unitOfWork.Product.Delete(product);
+            await _unitOfWork.SaveAsync();
+            TempData["Success"] = "Cafe deleted successfully";
+
+            return RedirectToAction("List");
+        }
+
     }
 }
